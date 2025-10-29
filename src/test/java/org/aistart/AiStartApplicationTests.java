85287e1878422dd221b151d82fdf185700eaaec9
sxwpskd <ddf8797319@126.com>
2025-10-29 20:20:06 +0800

    vesion-start

---
+++ src/test/java/org/aistart/AiStartApplicationTests.java
@@ -0,0 +1,13 @@
+package org.aistart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiStartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
