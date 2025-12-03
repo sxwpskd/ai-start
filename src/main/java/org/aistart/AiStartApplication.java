6893114ec99e6eb9803a38677d004e9654419149
sxwpskd <ddf8797319@126.com>
2025-12-03 21:18:46 +0800

    第6期-后端完成配置

--- src/main/java/org/aistart/AiStartApplication.java
+++ src/main/java/org/aistart/AiStartApplication.java
@@ -1,10 +1,12 @@
 package org.aistart;
 
+import dev.langchain4j.community.store.embedding.redis.spring.RedisEmbeddingStoreAutoConfiguration;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {RedisEmbeddingStoreAutoConfiguration.class})
+//redis默认开启向量增强功能，在rag增强会用到，本项目用不到，所以在这里关闭
 @MapperScan("org.aistart.mapper")
 public class  AiStartApplication {
 
